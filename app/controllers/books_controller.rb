ad40b29fb68c1398d9e69a0bc1fa07c65215c5d9
yuhidomoto <yuhi4424@yahoo.co.jp>
2020-01-18 15:43:15 +0900

    課題提出2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,19 +11,33 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+
+     redirect_to book_path(@book.id)
+     flash[:notice] = "Book was successfully created."
+    else
+      @books = Book.all
+      render action: :index
+   end
   end
+
 
   def edit
     @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+    else
+      @books = Book.all
+      render action: :index
+   end
   end
+
 
   def destroy
   	book = Book.find(params[:id])
